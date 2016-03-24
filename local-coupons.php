50bc12aaed787b01e4427f4b851a48192d5eb092
testing-the-waters12 <Ahmed.Hammad@rtuihou.com>
2016-03-24 09:56:57 -0500

    temporarily disabled seo-friendly route until I improve the implementation

--- local-coupons.php
+++ local-coupons.php
@@ -10,8 +10,16 @@
 			'imageBase' => 'http://www.rtui.com/uploads/couponsthumb',
 			'getShortcut' => function($value) {
 				$alias = basename($value->alias);
-				return "view-coupon.php/$alias.html";
-				//return "view-coupon.php?id=$alias";
+
+				/**
+				* 2016-03-24 INFO: AHH
+				* I'm disabling the SEO friendly URL because it changes the baseline URL.  The
+				* obvious way around it is to use absolute URLs, but I don't want other maintainers
+				* to have to worry about that, so until I change the implementation (e.g.: .htaccess)
+				* I'm using query parameters.
+				*/
+				//return "view-coupon.php/$alias.html";
+				return "view-coupon.php?id=$alias";
 			}
 		)
 	);
